c2ec5e1155234e1871677d196ec8956f1ef1b1ce
md-siam-islam <mdsiamislam663@gmail.com>
2025-01-19 10:56:07 +0600

    All Newsletter subscribers table formate desugn done

--- src/DashBoard/AdminPageSection/Newslettersubscribers/Newslettersubscribers.jsx
+++ src/DashBoard/AdminPageSection/Newslettersubscribers/Newslettersubscribers.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import useAxiosPublic from "../../../Components/UseAxiosPublic/useAxiosPublic";
 
 const Newslettersubscribers = () => {
-    return (
-        <div>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni velit est delectus debitis totam? Consectetur repudiandae ab, sequi ea inventore ut distinctio beatae rem aspernatur laborum amet repellat consequatur saepe et cumque error? Esse nisi sunt nobis voluptas dolorem eaque, molestias in quasi porro neque rerum mollitia. Expedita, quisquam repellendus! Eum rerum dolorum consequuntur beatae fuga, voluptatibus reiciendis expedita, dolore vitae voluptate enim, autem in earum aut. Totam expedita perferendis dolore inventore sequi, animi qui, explicabo facere nostrum libero alias!
-        </div>
-    );
+  const AxiosPublic = useAxiosPublic();
+  const [newsLatter, setNewsLatter] = useState([]);
+  useEffect(() => {
+    AxiosPublic.get("/newslate").then((res) => {
+      setNewsLatter(res.data);
+    });
+  }, [AxiosPublic]);
+  return (
+    <div>
+        <h1 className="text-2xl md:text-4xl my-6 underline
+         text-center font-bold">All Newsletter subscribers</h1>
+      <div className="overflow-x-auto">
+        <table className="table-auto w-full border border-gray-300">
+          <thead>
+            <tr className="bg-gray-100 border-b border-gray-300">
+              <th className="border border-gray-300 px-4 py-2">#</th>
+              <th className="border border-gray-300 px-4 py-2">Name</th>
+              <th className="border border-gray-300 px-4 py-2">Email</th>
+              <th className="border border-gray-300 px-4 py-2">
+                Subscription Date
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {newsLatter?.length > 0 ? (
+              newsLatter.map((subscriber, index) => (
+                <tr key={subscriber._id} className="border-b border-gray-300">
+                  <td className="border border-gray-300 px-4 py-2">
+                    {index + 1}
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    {subscriber.name}
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    {subscriber.email}
+                  </td>
+                  <td className="border border-gray-300 px-4 py-2">
+                    {new Date(subscriber.subscriptionDate).toLocaleString()}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td
+                  colSpan="4"
+                  className="text-center border border-gray-300 px-4 py-2"
+                >
+                  No subscribers found.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Newslettersubscribers;
